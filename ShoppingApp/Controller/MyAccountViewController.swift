ffa8dbbb324918388567bfb2a0da7d4a72e35748
ahmednasr8898 <ans96283@gmail.com>
2021-01-03 12:32:27 +0200

    StartWithMyAccountPage

---
+++ ShoppingApp/Controller/MyAccountViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MyAccountViewController.swift
+//  ShoppingApp
+//
+//  Created by Ahmed Nasr on 1/3/21.
+//
+import UIKit
+
+class MyAccountViewController: UIViewController {
+
+    @IBOutlet weak var signInView: UIView!
+    @IBOutlet weak var signUpView: UIStackView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+}
