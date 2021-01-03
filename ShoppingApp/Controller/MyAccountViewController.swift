bcc5e9ab6cdc7c4296664228dc2219f9af9b4aab
ahmednasr8898 <ans96283@gmail.com>
2021-01-03 15:30:36 +0200

    EditInMyAccountPage

--- ShoppingApp/Controller/MyAccountViewController.swift
+++ ShoppingApp/Controller/MyAccountViewController.swift
@@ -8,9 +8,26 @@ import UIKit
 
 class MyAccountViewController: UIViewController {
 
+    @IBOutlet weak var signinView: UIView!
+    @IBOutlet weak var signupView: UIView!
+    @IBOutlet weak var countryView: UIView!
     
+    @IBOutlet weak var languageView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        addActionToView()
+    }
+    func addActionToView(){
+        let signInGesture = UITapGestureRecognizer(target: self, action: #selector(signinOnClick(sender:)))
+        signinView.addGestureRecognizer(signInGesture)
+        
+        let signUpGesture = UITapGestureRecognizer(target: self, action: #selector(signupOnClick(sender:)))
+        signupView.addGestureRecognizer(signUpGesture)
+    }
+    @objc func signinOnClick(sender: UITapGestureRecognizer){
+        print("go to sign in page")
+    }
+    @objc func signupOnClick(sender: UITapGestureRecognizer){
+        print("go to sign up page")
     }
 }
