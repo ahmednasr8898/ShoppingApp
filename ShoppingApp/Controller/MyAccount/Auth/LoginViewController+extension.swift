8f2b991c38276cc679fb0e6188c9d4887d82d50b
ahmednasr8898 <ans96283@gmail.com>
2021-01-04 17:11:39 +0200

    FinishLoginPage

--- ShoppingApp/Controller/MyAccount/Auth/LoginViewController+extension.swift
+++ ShoppingApp/Controller/MyAccount/Auth/LoginViewController+extension.swift
@@ -24,4 +24,62 @@ extension LoginViewController{
         cancelImage.translatesAutoresizingMaskIntoConstraints = false
         cancelImage.widthAnchor.constraint(equalToConstant: view.frame.width * 0.06).isActive = true
     }
+    func setUpAhlanLabel(){
+        view.addSubview(ahlanLabel)
+        ahlanLabel.translatesAutoresizingMaskIntoConstraints = false
+        ahlanLabel.topAnchor.constraint(equalTo: topStackView.bottomAnchor, constant: 12).isActive = true
+        ahlanLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
+    func setUpEmailStackView(){
+        view.addSubview(emailStackView)
+        emailStackView.translatesAutoresizingMaskIntoConstraints = false
+        emailStackView.topAnchor.constraint(equalTo: ahlanLabel.bottomAnchor, constant: 32).isActive = true
+        emailStackView.widthAnchor.constraint(equalToConstant: view.frame.width * 0.8).isActive = true
+        emailStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        emailStackView.addArrangedSubview(emailLabel)
+        emailStackView.addArrangedSubview(emailTextField)
+      
+        emailTextField.translatesAutoresizingMaskIntoConstraints = false
+        emailTextField.heightAnchor.constraint(equalToConstant: 42).isActive = true
+    }
+    func setUpPasswordStackView(){
+        view.addSubview(passwordStackView)
+        passwordStackView.translatesAutoresizingMaskIntoConstraints = false
+        passwordStackView.topAnchor.constraint(equalTo: emailStackView.bottomAnchor, constant: 32).isActive = true
+        passwordStackView.widthAnchor.constraint(equalTo: emailStackView.widthAnchor).isActive = true
+        passwordStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+
+        passwordStackView.addArrangedSubview(passwordLabel)
+        passwordStackView.addArrangedSubview(passwordTextField)
+        
+        passwordTextField.translatesAutoresizingMaskIntoConstraints = false
+        passwordTextField.heightAnchor.constraint(equalToConstant: 42).isActive = true
+    }
+    func setUpShowPassword(){
+        passwordTextField.addSubview(showPasswordButton)
+        showPasswordButton.translatesAutoresizingMaskIntoConstraints = false
+        showPasswordButton.centerYAnchor.constraint(equalTo: passwordTextField.centerYAnchor).isActive = true
+        showPasswordButton.trailingAnchor.constraint(equalTo: passwordTextField.trailingAnchor, constant: -6).isActive = true
+    }
+    func setUpForgotPassword(){
+        view.addSubview(forgotButton)
+        forgotButton.translatesAutoresizingMaskIntoConstraints = false
+        forgotButton.topAnchor.constraint(equalTo: passwordStackView.bottomAnchor, constant: 30).isActive = true
+        forgotButton.leadingAnchor.constraint(equalTo: passwordTextField.leadingAnchor).isActive = true
+    }
+    func setUpSigninButton(){
+        view.addSubview(signinButton)
+        signinButton.translatesAutoresizingMaskIntoConstraints = false
+        signinButton.topAnchor.constraint(equalTo: forgotButton.bottomAnchor, constant: 25).isActive = true
+        signinButton.widthAnchor.constraint(equalToConstant: view.frame.width * 0.8).isActive = true
+        signinButton.heightAnchor.constraint(equalToConstant: view.frame.height * 0.07).isActive = true
+        signinButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
+    func setUpHaveAccountButton(){
+        view.addSubview(haveAccountButton)
+        haveAccountButton.translatesAutoresizingMaskIntoConstraints = false
+        haveAccountButton.topAnchor.constraint(equalTo: signinButton.bottomAnchor, constant: 30).isActive = true
+        haveAccountButton.leadingAnchor.constraint(equalTo: emailStackView.leadingAnchor).isActive = true
+    }
 }
--- ShoppingApp/Controller/MyAccount/Auth/LoginViewController.swift
+++ ShoppingApp/Controller/MyAccount/Auth/LoginViewController.swift
@@ -20,7 +20,6 @@ class LoginViewController: UIViewController {
         img.image = #imageLiteral(resourceName: "noon")
         return img
     }()
-    
     let cancelImage: UIImageView = {
        let img = UIImageView()
         img.contentMode = .scaleAspectFit
@@ -28,13 +27,103 @@ class LoginViewController: UIViewController {
         img.tintColor = #colorLiteral(red: 0.2383217293, green: 0.2383217293, blue: 0.2383217293, alpha: 1)
         return img
     }()
+    let ahlanLabel: UILabel = {
+        let lbl = UILabel()
+        lbl.text = "Ahlan! Welcome back!"
+        lbl.font = UIFont.systemFont(ofSize: 28)
+        lbl.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        return lbl
+    }()
+    let emailStackView: UIStackView = {
+        let st = UIStackView()
+        st.axis = .vertical
+        st.distribution = .fillEqually
+        st.spacing = 12
+        return st
+    }()
+    let emailLabel: UILabel = {
+        let lbl = UILabel()
+        lbl.text = "Email"
+        lbl.font = UIFont.systemFont(ofSize: 13)
+        lbl.textColor = #colorLiteral(red: 0.7979679404, green: 0.7979679404, blue: 0.7979679404, alpha: 1)
+        return lbl
+    }()
+    let emailTextField: UITextField = {
+        let txt = UITextField()
+        txt.placeholder = "Please enter your email address"
+        return txt
+    }()
+    let passwordStackView: UIStackView = {
+        let st = UIStackView()
+        st.axis = .vertical
+        st.distribution = .fillEqually
+        st.spacing = 12
+        return st
+    }()
+    let passwordLabel: UILabel = {
+        let lbl = UILabel()
+        lbl.text = "Password"
+        lbl.font = UIFont.systemFont(ofSize: 13)
+        lbl.textColor = #colorLiteral(red: 0.7979679404, green: 0.7979679404, blue: 0.7979679404, alpha: 1)
+        return lbl
+    }()
+    let passwordTextField: UITextField = {
+        let txt = UITextField()
+        txt.placeholder = "Please enter your password"
+        txt.isSecureTextEntry = true
+        return txt
+    }()
+    let showPasswordButton: UIButton = {
+        let btn = UIButton()
+        btn.setImage(UIImage(systemName: "eye"), for: .normal)
+        btn.tintColor = UIColor.black
+        btn.addTarget(self, action: #selector(showPasswordOnClick(sender:)), for: .touchUpInside)
+        return btn
+    }()
+    let forgotButton: UIButton = {
+        let btn = UIButton()
+        let attributedString = NSAttributedString(string: "Forgot your password?", attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 20), NSAttributedString.Key.foregroundColor: #colorLiteral(red: 0.5442033679, green: 0.5442033679, blue: 0.5442033679, alpha: 1)])
+        btn.setAttributedTitle(attributedString, for: .normal)
+        return btn
+    }()
+    let signinButton: UIButton = {
+        let btn = UIButton()
+        let attributedString = NSAttributedString(string: "Sign In", attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 14), NSAttributedString.Key.foregroundColor: UIColor.white])
+        btn.setAttributedTitle(attributedString, for: .normal)
+        btn.backgroundColor = #colorLiteral(red: 0.1019607857, green: 0.2784313858, blue: 0.400000006, alpha: 1)
+        return btn
+    }()
+    let haveAccountButton: UIButton = {
+        let btn = UIButton()
+        let attributedString = NSMutableAttributedString(string: "Dont't have an account? ", attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 17), NSAttributedString.Key.foregroundColor: #colorLiteral(red: 0.606359294, green: 0.606359294, blue: 0.606359294, alpha: 1)])
+        attributedString.append(NSAttributedString(string: "sign Up", attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 17), NSAttributedString.Key.foregroundColor: UIColor.black]))
+        btn.setAttributedTitle(attributedString, for: .normal)
+        return btn
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpTopStackVeiw()
         setUpNoonImage()
         setUpCancelImage()
-    
-
+        setUpAhlanLabel()
+        setUpEmailStackView()
+        setUpPasswordStackView()
+        setUpForgotPassword()
+        setUpSigninButton()
+        setUpHaveAccountButton()
+        setUpShowPassword()
+    }
+    @objc func showPasswordOnClick(sender: UIButton){
+        sender.isSelected = !sender.isSelected
+        if sender.isSelected{
+            print("selected")
+            showPasswordButton.setImage(UIImage(systemName: "eye.slash"), for: .normal)
+            passwordTextField.isSecureTextEntry = false
+        }else{
+            print("none selected")
+            showPasswordButton.setImage(UIImage(systemName: "eye"), for: .normal)
+            passwordTextField.isSecureTextEntry = true
+        }
     }
 }
