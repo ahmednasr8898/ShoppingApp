69943c2957daf4cdd646e78e32e071c0c671bee6
ahmednasr8898 <ans96283@gmail.com>
2021-01-05 18:39:10 +0200

    RefactorInRegisterPage

--- ShoppingApp/Controller/MyAccount/Auth/RegisterViewController+extension.swift
+++ ShoppingApp/Controller/MyAccount/Auth/RegisterViewController+extension.swift
@@ -7,6 +7,10 @@
 import UIKit
 extension RegisterViewController{
     func setUpTopStackVeiw(){
+        //Properties
+        topStackView.axis = .horizontal
+        topStackView.distribution = .equalSpacing
+        //Constraint
         view.addSubview(topStackView)
         topStackView.translatesAutoresizingMaskIntoConstraints = false
         topStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant:
@@ -16,16 +20,30 @@ extension RegisterViewController{
         topStackView.heightAnchor.constraint(equalToConstant: view.frame.height * 0.08).isActive = true
     }
     func setUpNoonImage(){
+        //Properties
+        noonImageView.contentMode = .scaleAspectFit
+        noonImageView.image = #imageLiteral(resourceName: "noon")
+        //Constraint
         topStackView.addArrangedSubview(noonImageView)
         noonImageView.translatesAutoresizingMaskIntoConstraints = false
         noonImageView.widthAnchor.constraint(equalToConstant: view.frame.width * 0.26).isActive = true
     }
     func setUpCancelImage(){
+        //Properties
+        cancelImage.contentMode = .scaleAspectFit
+        cancelImage.image = UIImage(systemName: "xmark")
+        cancelImage.tintColor = #colorLiteral(red: 0.2383217293, green: 0.2383217293, blue: 0.2383217293, alpha: 1)
+        //Constraint
         topStackView.addArrangedSubview(cancelImage)
         cancelImage.translatesAutoresizingMaskIntoConstraints = false
         cancelImage.widthAnchor.constraint(equalToConstant: view.frame.width * 0.06).isActive = true
     }
     func setUpCreateNoonLabel(){
+        //Properties
+        createnoonLabel.text = "Create a noon account"
+        createnoonLabel.font = UIFont.systemFont(ofSize: 28)
+        createnoonLabel.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        //Constraint
         view.addSubview(createnoonLabel)
         createnoonLabel.translatesAutoresizingMaskIntoConstraints = false
         createnoonLabel.topAnchor.constraint(equalTo: topStackView.bottomAnchor, constant:
@@ -33,74 +51,149 @@ extension RegisterViewController{
         createnoonLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
     }
     func setUpFirstNameStackView(){
+        //Properties
+        firstNameStackView.axis = .vertical
+        firstNameStackView.distribution = .fillEqually
+        firstNameStackView.spacing = 12
+        //Constraint
         view.addSubview(firstNameStackView)
         firstNameStackView.translatesAutoresizingMaskIntoConstraints = false
         firstNameStackView.topAnchor.constraint(equalTo: createnoonLabel.bottomAnchor, constant:
                                                 view.frame.height * 32/896).isActive = true
         firstNameStackView.widthAnchor.constraint(equalToConstant: view.frame.width * 0.8).isActive = true
         firstNameStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        
         firstNameStackView.addArrangedSubview(firstNameLabel)
         firstNameStackView.addArrangedSubview(firstNameTextField)
-      
+    }
+    func setUpFirstNameLabel(){
+        //Properties
+        firstNameLabel.text = "First Name"
+        firstNameLabel.font = UIFont.systemFont(ofSize: 15)
+        firstNameLabel.textColor = #colorLiteral(red: 0.606359294, green: 0.5660213921, blue: 0.1940417385, alpha: 1)
+    }
+    func setUpFirstNameTextField(){
+        //Properties
+        firstNameTextField.placeholder = "Please enter your first name"
+        //Constraint
         firstNameTextField.translatesAutoresizingMaskIntoConstraints = false
         firstNameTextField.heightAnchor.constraint(equalToConstant:
                                                 view.frame.height * 42/896).isActive = true
     }
     func setUpLastNameStackView(){
+        //Properties
+        lastNameStackView.axis = .vertical
+        lastNameStackView.distribution = .fillEqually
+        lastNameStackView.spacing = 12
+        //Constraint
         view.addSubview(lastNameStackView)
         lastNameStackView.translatesAutoresizingMaskIntoConstraints = false
         lastNameStackView.topAnchor.constraint(equalTo: firstNameStackView.bottomAnchor, constant:
                                                 view.frame.height * 32/896).isActive = true
         lastNameStackView.widthAnchor.constraint(equalTo: firstNameStackView.widthAnchor).isActive = true
         lastNameStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-
         lastNameStackView.addArrangedSubview(lastNameLabel)
         lastNameStackView.addArrangedSubview(lastNameTextField)
-        
+    }
+    func setUpLastNameLabel(){
+        //Properties
+        lastNameLabel.text = "Last Name"
+        lastNameLabel.font = UIFont.systemFont(ofSize: 15)
+        lastNameLabel.textColor = #colorLiteral(red: 0.606359294, green: 0.5660213921, blue: 0.1940417385, alpha: 1)
+    }
+    func setUpLastNameTextField(){
+        //Properties
+        lastNameTextField.placeholder = "Please enter your last name"
+        //Constraint
         lastNameTextField.translatesAutoresizingMaskIntoConstraints = false
         lastNameTextField.heightAnchor.constraint(equalToConstant:
                                                     view.frame.height * 42/896).isActive = true
     }
-    
     func setUpEmailStackView(){
+        //Properties
+        emailStackView.axis = .vertical
+        emailStackView.distribution = .fillEqually
+        emailStackView.spacing = 12
+        //Constraint
         view.addSubview(emailStackView)
         emailStackView.translatesAutoresizingMaskIntoConstraints = false
         emailStackView.topAnchor.constraint(equalTo: lastNameStackView.bottomAnchor, constant:
                                                 view.frame.height * 32/896).isActive = true
         emailStackView.widthAnchor.constraint(equalToConstant: view.frame.width * 0.8).isActive = true
         emailStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        
         emailStackView.addArrangedSubview(emailLabel)
         emailStackView.addArrangedSubview(emailTextField)
-      
+    }
+    func setUpEmailLabel(){
+        //Properties
+        emailLabel.text = "Email"
+        emailLabel.font = UIFont.systemFont(ofSize: 15)
+        emailLabel.textColor = #colorLiteral(red: 0.606359294, green: 0.5660213921, blue: 0.1940417385, alpha: 1)
+    }
+    func setUpEmailTextField(){
+        //Properties
+        emailTextField.placeholder = "Please enter your email address"
+        //Constraint
         emailTextField.translatesAutoresizingMaskIntoConstraints = false
         emailTextField.heightAnchor.constraint(equalToConstant:
                                                 view.frame.height * 42/896).isActive = true
     }
-    
     func setUpPasswordStackView(){
+        //Properties
+        passwordStackView.axis = .vertical
+        passwordStackView.distribution = .fillEqually
+        passwordStackView.spacing = 12
+        //Constraint
         view.addSubview(passwordStackView)
         passwordStackView.translatesAutoresizingMaskIntoConstraints = false
         passwordStackView.topAnchor.constraint(equalTo: emailStackView.bottomAnchor, constant:
                                                 view.frame.height * 32/896).isActive = true
         passwordStackView.widthAnchor.constraint(equalTo: emailStackView.widthAnchor).isActive = true
         passwordStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-
         passwordStackView.addArrangedSubview(passwordLabel)
         passwordStackView.addArrangedSubview(passwordTextField)
-        
+    }
+    func setUpPasswordLabel(){
+        //Properties
+        passwordLabel.text = "Password"
+        passwordLabel.font = UIFont.systemFont(ofSize: 15)
+        passwordLabel.textColor = #colorLiteral(red: 0.606359294, green: 0.5660213921, blue: 0.1940417385, alpha: 1)
+    }
+    func setUpPasswordTextField(){
+        //Properties
+        passwordTextField.placeholder = "Please enter your password"
+        //Constraint
         passwordTextField.translatesAutoresizingMaskIntoConstraints = false
         passwordTextField.heightAnchor.constraint(equalToConstant:
                                                     view.frame.height * 42/896).isActive = true
     }
     func setUpShowPassword(){
+        //Properties
+        showPasswordButton.setImage(UIImage(systemName: "eye"), for: .normal)
+        showPasswordButton.tintColor = UIColor.black
+        showPasswordButton.addTarget(self, action: #selector(showPasswordOnClick(sender:)), for: .touchUpInside)
+        //Constraint
         passwordTextField.addSubview(showPasswordButton)
         showPasswordButton.translatesAutoresizingMaskIntoConstraints = false
         showPasswordButton.centerYAnchor.constraint(equalTo: passwordTextField.centerYAnchor).isActive = true
         showPasswordButton.trailingAnchor.constraint(equalTo: passwordTextField.trailingAnchor, constant: -6).isActive = true
     }
+    @objc func showPasswordOnClick(sender: UIButton){
+        sender.isSelected = !sender.isSelected
+        if sender.isSelected{
+            showPasswordButton.setImage(UIImage(systemName: "eye.slash"), for: .normal)
+            passwordTextField.isSecureTextEntry = false
+        }else{
+            showPasswordButton.setImage(UIImage(systemName: "eye"), for: .normal)
+            passwordTextField.isSecureTextEntry = true
+        }
+    }
     func setUpSignupButton(){
+        //Properties
+        let font = UIFont(name: "HelveticaNeue-Bold", size: 14)
+        let attributedString = NSAttributedString(string: "Sign Up", attributes: [NSAttributedString.Key.font : font as Any, NSAttributedString.Key.foregroundColor: UIColor.white])
+        signupButton.setAttributedTitle(attributedString, for: .normal)
+        signupButton.backgroundColor = #colorLiteral(red: 0.1019607857, green: 0.2784313858, blue: 0.400000006, alpha: 1)
+        //Constraint
         view.addSubview(signupButton)
         signupButton.translatesAutoresizingMaskIntoConstraints = false
         signupButton.topAnchor.constraint(equalTo: passwordStackView.bottomAnchor, constant:
@@ -110,6 +203,11 @@ extension RegisterViewController{
         signupButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
     }
     func setUpHaveAccountButton(){
+        //Properties
+        let attributedString = NSMutableAttributedString(string: "Already have an account? ", attributes:[NSAttributedString.Key.font : UIFont.systemFont(ofSize: 17), NSAttributedString.Key.foregroundColor: #colorLiteral(red: 0.4665843588, green: 0.4665843588, blue: 0.4665843588, alpha: 1)])
+        attributedString.append(NSAttributedString(string: "sign In", attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 17), NSAttributedString.Key.foregroundColor: UIColor.black]))
+        haveAccountButton.setAttributedTitle(attributedString, for: .normal)
+        //Constraint
         view.addSubview(haveAccountButton)
         haveAccountButton.translatesAutoresizingMaskIntoConstraints = false
         haveAccountButton.topAnchor.constraint(equalTo: signupButton.bottomAnchor, constant:
--- ShoppingApp/Controller/MyAccount/Auth/RegisterViewController.swift
+++ ShoppingApp/Controller/MyAccount/Auth/RegisterViewController.swift
@@ -8,133 +8,26 @@ import UIKit
 
 class RegisterViewController: UIViewController {
     
-    let topStackView: UIStackView = {
-       let st = UIStackView()
-        st.axis = .horizontal
-        st.distribution = .equalSpacing
-        return st
-    }()
-    let noonImageView: UIImageView = {
-        let img = UIImageView()
-        img.contentMode = .scaleAspectFit
-        img.image = #imageLiteral(resourceName: "noon")
-        return img
-    }()
-    let cancelImage: UIImageView = {
-       let img = UIImageView()
-        img.contentMode = .scaleAspectFit
-        img.image = UIImage(systemName: "xmark")
-        img.tintColor = #colorLiteral(red: 0.2383217293, green: 0.2383217293, blue: 0.2383217293, alpha: 1)
-        return img
-    }()
-    let createnoonLabel: UILabel = {
-        let lbl = UILabel()
-        lbl.text = "Create a noon account"
-        lbl.font = UIFont.systemFont(ofSize: 28)
-        lbl.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
-        return lbl
-    }()
-    let firstNameStackView: UIStackView = {
-        let st = UIStackView()
-        st.axis = .vertical
-        st.distribution = .fillEqually
-        st.spacing = 12
-        return st
-    }()
-    let firstNameLabel: UILabel = {
-        let lbl = UILabel()
-        lbl.text = "First Name"
-        lbl.font = UIFont.systemFont(ofSize: 15)
-        lbl.textColor = #colorLiteral(red: 0.606359294, green: 0.5660213921, blue: 0.1940417385, alpha: 1)
-        return lbl
-    }()
-    let firstNameTextField: UITextField = {
-        let txt = UITextField()
-        txt.placeholder = "Please enter your first name"
-        return txt
-    }()
-    let lastNameStackView: UIStackView = {
-        let st = UIStackView()
-        st.axis = .vertical
-        st.distribution = .fillEqually
-        st.spacing = 12
-        return st
-    }()
-    let lastNameLabel: UILabel = {
-        let lbl = UILabel()
-        lbl.text = "Last Name"
-        lbl.font = UIFont.systemFont(ofSize: 15)
-        lbl.textColor = #colorLiteral(red: 0.606359294, green: 0.5660213921, blue: 0.1940417385, alpha: 1)
-        return lbl
-    }()
-    let lastNameTextField: UITextField = {
-        let txt = UITextField()
-        txt.placeholder = "Please enter your last name"
-        return txt
-    }()
-    let emailStackView: UIStackView = {
-        let st = UIStackView()
-        st.axis = .vertical
-        st.distribution = .fillEqually
-        st.spacing = 12
-        return st
-    }()
-    let emailLabel: UILabel = {
-        let lbl = UILabel()
-        lbl.text = "Email"
-        lbl.font = UIFont.systemFont(ofSize: 15)
-        lbl.textColor = #colorLiteral(red: 0.606359294, green: 0.5660213921, blue: 0.1940417385, alpha: 1)
-        return lbl
-    }()
-    let emailTextField: UITextField = {
-        let txt = UITextField()
-        txt.placeholder = "Please enter your email address"
-        return txt
-    }()
-    let passwordStackView: UIStackView = {
-        let st = UIStackView()
-        st.axis = .vertical
-        st.distribution = .fillEqually
-        st.spacing = 12
-        return st
-    }()
-    let passwordLabel: UILabel = {
-        let lbl = UILabel()
-        lbl.text = "Password"
-        lbl.font = UIFont.systemFont(ofSize: 15)
-        lbl.textColor = #colorLiteral(red: 0.606359294, green: 0.5660213921, blue: 0.1940417385, alpha: 1)
-        return lbl
-    }()
-    let passwordTextField: UITextField = {
-        let txt = UITextField()
-        txt.placeholder = "Please enter your password"
-        txt.isSecureTextEntry = true
-       
-        return txt
-    }()
-    let showPasswordButton: UIButton = {
-        let btn = UIButton()
-        btn.setImage(UIImage(systemName: "eye"), for: .normal)
-        btn.tintColor = UIColor.black
-        btn.addTarget(self, action: #selector(showPasswordOnClick(sender:)), for: .touchUpInside)
-        return btn
-    }()
-    let signupButton: UIButton = {
-        let btn = UIButton()
-        let font = UIFont(name: "HelveticaNeue-Bold", size: 14)
-        let attributedString = NSAttributedString(string: "Sign Up", attributes: [NSAttributedString.Key.font : font as Any, NSAttributedString.Key.foregroundColor: UIColor.white])
-        btn.setAttributedTitle(attributedString, for: .normal)
-        btn.backgroundColor = #colorLiteral(red: 0.1019607857, green: 0.2784313858, blue: 0.400000006, alpha: 1)
-        return btn
-    }()
-    let haveAccountButton: UIButton = {
-        let btn = UIButton()
-        let attributedString = NSMutableAttributedString(string: "Already have an account? ", attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 17), NSAttributedString.Key.foregroundColor: #colorLiteral(red: 0.4665843588, green: 0.4665843588, blue: 0.4665843588, alpha: 1)])
-        attributedString.append(NSAttributedString(string: "sign In", attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 17), NSAttributedString.Key.foregroundColor: UIColor.black]))
-        btn.setAttributedTitle(attributedString, for: .normal)
-        return btn
-    }()
-    
+    let topStackView = UIStackView()
+    let noonImageView = UIImageView()
+    let cancelImage = UIImageView()
+    let createnoonLabel = UILabel()
+    let firstNameStackView = UIStackView()
+    let firstNameLabel = UILabel()
+    let firstNameTextField = UITextField()
+    let lastNameStackView = UIStackView()
+    let lastNameLabel = UILabel()
+    let lastNameTextField = UITextField()
+    let emailStackView = UIStackView()
+    let emailLabel = UILabel()
+    let emailTextField = UITextField()
+    let passwordStackView = UIStackView()
+    let passwordLabel = UILabel()
+    let passwordTextField = UITextField()
+    let showPasswordButton = UIButton()
+    let signupButton = UIButton()
+    let haveAccountButton = UIButton()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpTopStackVeiw()
@@ -142,22 +35,19 @@ class RegisterViewController: UIViewController {
         setUpCancelImage()
         setUpCreateNoonLabel()
         setUpFirstNameStackView()
+        setUpFirstNameLabel()
+        setUpFirstNameTextField()
         setUpLastNameStackView()
+        setUpLastNameLabel()
+        setUpLastNameTextField()
         setUpEmailStackView()
+        setUpEmailLabel()
+        setUpEmailTextField()
         setUpPasswordStackView()
+        setUpPasswordLabel()
+        setUpPasswordTextField()
         setUpSignupButton()
         setUpHaveAccountButton()
         setUpShowPassword()
-        
-    }
-    @objc func showPasswordOnClick(sender: UIButton){
-        sender.isSelected = !sender.isSelected
-        if sender.isSelected{
-            showPasswordButton.setImage(UIImage(systemName: "eye.slash"), for: .normal)
-            passwordTextField.isSecureTextEntry = false
-        }else{
-            showPasswordButton.setImage(UIImage(systemName: "eye"), for: .normal)
-            passwordTextField.isSecureTextEntry = true
-        }
     }
 }
